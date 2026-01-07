9055fffc3a8627632bd0429a78a46334bb949a16
Swayam Gandhi <swayamgandhi6@gmail.com>
2026-01-07 13:29:12 +0530

    made apis

---
+++ src/api/sold-property/controllers/sold-property.ts
@@ -0,0 +1,7 @@
+/**
+ * sold-property controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::sold-property.sold-property');
---
+++ src/api/sold-property/services/sold-property.ts
@@ -0,0 +1,7 @@
+/**
+ * sold-property service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sold-property.sold-property');
